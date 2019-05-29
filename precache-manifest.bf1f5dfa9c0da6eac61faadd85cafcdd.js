279b78abe9bbdc1d6562da9a96ed35c4842a3e16
Riijohanna <riikka.jharju@gmail.com>
2019-05-29 11:23:33 +0300

    Updates

---
+++ precache-manifest.bf1f5dfa9c0da6eac61faadd85cafcdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6764f4d73c15bec1e7c6e7c264f5292",
+    "url": "/index.html"
+  },
+  {
+    "revision": "422f0c191e1f928777a4",
+    "url": "/static/css/main.f1082aaf.chunk.css"
+  },
+  {
+    "revision": "fadbb0705bdd11d4fb8f",
+    "url": "/static/js/2.5d907561.chunk.js"
+  },
+  {
+    "revision": "422f0c191e1f928777a4",
+    "url": "/static/js/main.999e2b3d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
